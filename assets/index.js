d55c6fc2176be82274ab2688269bd08358d7a3eb
ItayFrisk <itay.manevych@gmail.com>
2022-12-13 19:00:08 +0200

    (patch) changed the initialization of the board

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,10 @@
-var userLife = 200;
-var monsterLife = 50;
-var monsterDamage = 10;
-var userDamage = 5;
+let userLife = 200;
+let monsterLife = 50;
+let monsterDamage = 10;
+let userDamage = 5;
 
 // Create the board as a boardSize x boardSize two-dimensional array
-const boardSize = 15;
+const boardSize = 25;
 let board = new Array(boardSize);
 for (let i = 0; i < boardSize; i++) {
 	board[i] = new Array(boardSize);
@@ -13,22 +13,19 @@ for (let i = 0; i < boardSize; i++) {
 // Initialize the board with players and monsters
 for (let i = 0; i < boardSize; i++) {
 	for (let j = 0; j < boardSize; j++) {
-		if (i === 0 && j === 0) {
-			board[i][j] = "player";
-		} else if (i === (boardSize-1) && j === (boardSize-1)) {
-			board[i][j] = "finish line";
-		} else {
-			const randNum = Math.floor(Math.random() * 4) + 1;
-			if(randNum == 2){
-				board[i][j] = "item";
-			}else if(randNum == 3){
-				board[i][j] = "monster";
-			}else{
-				board[i][j] = "empty";
-			}
+		const randNum = Math.floor(Math.random() * 4) + 1;
+		if(randNum == 2){
+			board[i][j] = "item";
+		}else if(randNum == 3){
+			board[i][j] = "monster";
+		}else{
+			board[i][j] = "empty";
 		}
 	}
 }
+board[0][0] = "player";
+board[boardSize-1][boardSize-1] = "finish line";
+
 console.log(board);
 function myLoop() {
 	setTimeout(function() {
