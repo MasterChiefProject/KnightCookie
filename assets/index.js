22df8fa23eca771612685f08956ac5dd66a5e93b
Master Chief <46121368+masterchiefe343@users.noreply.github.com>
2022-12-15 16:42:20 +0200

    (patch)
    -> changed pictures extension endings to .webp
    -> changed code order

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,31 @@
+//////////////////////////////---VARIABLES---//////////////////////////////
 let userLife = 200;
 let userDamage = 5;
 let boardRows = 0;
 let boardCols = 0;
 let moveAbility = 1;
-const player = "<img src=\"assets/images/cookie.png\" width=\"25px\" height=\"25px\"/>"
+
+const player = "<img src=\"assets/images/cookie.webp\" width=\"25px\" height=\"25px\"/>"
 
 let monstersJson = '{ "monsters" : [' +
-'{ "id":"0" , "name":"Broccoli" , "img":"assets/images/broccoli.png" , "life":"80" , "damage":"25" },' +
-'{ "id":"1" , "name":"Carrot" , "img":"assets/images/carrot.gif" , "life":"120" , "damage":"5" },' +
-'{ "id":"2" , "name":"Beetroot" , "img":"assets/images/beetroot.png" , "life":"100" , "damage":"20" },' +
-'{ "id":"3" , "name":"Pepper" , "img":"assets/images/pepper.png" , "life":"110" , "damage":"45" },' +
-'{ "id":"4" , "name":"Spinach" , "img":"assets/images/spinach.jpg" , "life":"160" , "damage":"35" },' +
-'{ "id":"5" , "name":"Avocado" , "img":"assets/images/avocado.png" , "life":"170" , "damage":"40" },' +
+'{ "id":"0" , "name":"Broccoli" , "img":"assets/images/broccoli.webp" , "life":"80" , "damage":"25" },' +
+'{ "id":"1" , "name":"Carrot" , "img":"assets/images/carrot.webp" , "life":"120" , "damage":"5" },' +
+'{ "id":"2" , "name":"Beetroot" , "img":"assets/images/beetroot.webp" , "life":"100" , "damage":"20" },' +
+'{ "id":"3" , "name":"Pepper" , "img":"assets/images/pepper.webp" , "life":"110" , "damage":"45" },' +
+'{ "id":"4" , "name":"Spinach" , "img":"assets/images/spinach.webp" , "life":"160" , "damage":"35" },' +
+'{ "id":"5" , "name":"Avocado" , "img":"assets/images/avocado.webp" , "life":"170" , "damage":"40" },' +
 '{ "id":"6" , "name":"Walnuts" , "img":"assets/images/walnut.webp" , "life":"70" , "damage":"50" },' +
 '{ "id":"7" , "name":"Apple" , "img":"assets/images/apple.webp" , "life":"300" , "damage":"30" },' +
-'{ "id":"8" , "name":"Gym" , "img":"assets/images/gym.png" , "life":"200" , "damage":"75" },' +
-'{ "id":"9" , "name":"Protein Bar" , "img":"assets/images/proteinbar.png" , "life":"100" , "damage":"140" } ]}';
+'{ "id":"8" , "name":"Gym" , "img":"assets/images/gym.webp" , "life":"200" , "damage":"75" },' +
+'{ "id":"9" , "name":"Protein Bar" , "img":"assets/images/proteinbar.webp" , "life":"100" , "damage":"140" } ]}';
 
+let itemsJson = '{ "items" : [' +
+'{ "id":"0" , "name":"MiniMarshmallows" , "img":"assets/images/minimarshmallows.webp" , "extraLife":"5" , "extraDamage":"5" },' +
+'{ "id":"1" , "name":"CrushedCandyCanes" , "img":"assets/images/crushedcandycanes.webp" , "extraLife":"10" , "extraDamage":"10" },' +
+'{ "id":"2" , "name":"Confetti" , "img":"assets/images/confetti.webp" , "extraLife":"15" , "extraDamage":"15" },' +
+'{ "id":"3" , "name":"Rainbow" , "img":"assets/images/dragees.webp" , "extraLife":"30" , "extraDamage":"30" } ]}';
+
+let pressed = false;
 const obj = JSON.parse(monstersJson);
 
 // Create the board as a boardSize x boardSize two-dimensional array
@@ -32,10 +41,10 @@ for (let i = 0; i < boardSize; i++) {
 		const randNum = Math.floor(Math.random() * 4) + 1;
 		if(randNum == 2){
 			const randItem = Math.floor(Math.random() * 10);
-			board[i][j] = "<img src=\"assets/images/questionMark.png\" width=\"25px\" height=\"25px\" style=\"position:absolute\"/> <span style=\"visibility: collapse;\"; class=\"item\"> item </span> <span style=\"visibility: hidden;\"; class=\"number\">" + obj.monsters[randItem].id + "</span>";
+			board[i][j] = "<img src=\"assets/images/questionMark.webp\" width=\"25px\" height=\"25px\" style=\"position:absolute\"/> <span style=\"visibility: collapse;\"; class=\"item\"> item </span> <span style=\"visibility: hidden;\"; class=\"number\">" + obj.monsters[randItem].id + "</span>";
 		}else if(randNum == 3){
 			const randMon = Math.floor(Math.random() * 10);
-			board[i][j] = "<img src=\"assets/images/questionMark.png\" width=\"25px\" height=\"25px\" style=\"position:absolute\"/> <span style=\"visibility: collapse;\"; class=\"mosnter\"> monster </span> <span style=\"visibility: hidden;\"; class=\"number\">" + obj.monsters[randMon].id + "</span>";
+			board[i][j] = "<img src=\"assets/images/questionMark.webp\" width=\"25px\" height=\"25px\" style=\"position:absolute\"/> <span style=\"visibility: collapse;\"; class=\"mosnter\"> monster </span> <span style=\"visibility: hidden;\"; class=\"number\">" + obj.monsters[randMon].id + "</span>";
 		}else{
 			board[i][j] = "";
 		}
@@ -43,6 +52,48 @@ for (let i = 0; i < boardSize; i++) {
 }
 board[0][0] = player;
 board[boardSize-1][boardSize-1] = "finish line";
+//////////////////////////////---VARIABLES---//////////////////////////////
+
+
+//////////////////////////////------MAIN-----//////////////////////////////
+createTable(board);
+
+document.addEventListener('keydown', getInput);
+
+document.addEventListener('keyup' , isPressed);
+//////////////////////////////------MAIN-----//////////////////////////////
+
+
+
+//////////////////////////////---FUNCTIONS---//////////////////////////////
+function isPressed() {
+pressed = false; 
+}
+
+function getInput(event) {
+    if (pressed) return;
+   	pressed = true;
+
+	document.getElementById("table").remove();
+	
+    if (event.key === "ArrowLeft" || event.key === "a" || event.key === "ש") 
+		stepLeft(boardRows, boardCols);
+	
+
+    else if (event.key === "ArrowRight" || event.key === "d" || event.key === "ג")  
+		stepRight(boardRows, boardCols);
+	
+
+    else if (event.key === "ArrowUp" || event.key === "w" || event.key === "'") 
+		stepUp(boardRows,boardCols);
+	
+
+    else if (event.key === "ArrowDown" || event.key === "s" || event.key === "ד") 
+		stepDown(boardRows,boardCols);
+
+	createTable(board);
+}
+
 
 function createTable(tableData) {
 	let table = document.createElement('table');
@@ -66,7 +117,6 @@ function createTable(tableData) {
 	table.appendChild(tableBody);
 	document.getElementById('game').appendChild(table);
 }
-createTable(board);
 
 function typeCheck(rows, cols)
 {
@@ -133,42 +183,13 @@ function stepDown(rows,cols)
 	}
 }
 
-let pressed = false; 
-document.addEventListener('keydown', function(event) {
-    if (pressed) return;
-   	pressed = true;
-
-	document.getElementById("table").remove();
-	
-    if (event.key === "ArrowLeft" || event.key === "a" || event.key === "ש") 
-		stepLeft(boardRows, boardCols);
-	
-
-    else if (event.key === "ArrowRight" || event.key === "d" || event.key === "ג")  
-		stepRight(boardRows, boardCols);
-	
-
-    else if (event.key === "ArrowUp" || event.key === "w" || event.key === "'") 
-		stepUp(boardRows,boardCols);
-	
-
-    else if (event.key === "ArrowDown" || event.key === "s" || event.key === "ד") 
-		stepDown(boardRows,boardCols);
-
-	createTable(board);
-})
-
-document.addEventListener('keyup' , function(event) {
-    pressed = false;
-})
-
 function fight(i){
 	moveAbility = 0;
 	let monsterLife = obj.monsters[i].life;
 	let monsterDamage = obj.monsters[i].damage;
 	function fightArgmnt(){
 		setTimeout(function() {
-			document.getElementById("fight").innerHTML = "<div class=\"fightArgmntLeft\">Player Life: " + userLife + "<br /><img src=\"assets/images/cookie.png\" height=\"50px\" /><br />Power: " + userDamage + "</div><div class=\"fightArgmntRight\">Monster Life: " + monsterLife + "<br /><img src=\"" + obj.monsters[i].img + "\" height=\"50px\" /><br />Power: " + monsterDamage + "</div>";
+			document.getElementById("fight").innerHTML = "<div class=\"fightArgmntLeft\">Player Life: " + userLife + "<br /><img src=\"assets/images/cookie.webp\" height=\"50px\" /><br />Power: " + userDamage + "</div><div class=\"fightArgmntRight\">Monster Life: " + monsterLife + "<br /><img src=\"" + obj.monsters[i].img + "\" height=\"50px\" /><br />Power: " + monsterDamage + "</div>";
 			userLife -= monsterDamage;
 			monsterLife -= userDamage;
 			if (userLife > 0 && monsterLife > 0) {
@@ -190,5 +211,4 @@ function fight(i){
 	}
 	return 1;
 }
-
-
+//////////////////////////////---FUNCTIONS---//////////////////////////////
