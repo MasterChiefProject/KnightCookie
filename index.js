7e1ff5cab3f29c5a3c34f9a40483e17d984cbcb7
ItayFrisk <itay.manevych@gmail.com>
2023-01-03 19:30:06 +0200

    (patch) deleted unnecessary imports

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 import {
 	userLife,
 	userDamage,
-	boardSize,
-	boardRows,
-	boardCols,
-	moveAbility,
-	pressed,
-	player,
-	finishLine,
-	SetUserLife,
-	SetUserDamage,
-	SetBoardRows,
-	SetBoardCols,
-	SetMoveAbility,
-	SetPressed,
 } from './components/GlobalVariables.js';
 
 import {board} from './components/Board-Creation/Board.js';
-
-import {monstersJson, objMonster} from './components/JSON/Monsters.js';
-
-import {itemsJson, objItem} from './components/JSON/Items.js';
 
 import { createTable } from './components/Board-Visualization/CreateTable.js';
 import { CreateStat } from './components/Player-Visualization/CreateStat.js';
